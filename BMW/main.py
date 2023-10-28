856936e9a56051cfc7b4c30f9eb7a33e1686c2f1
Mksn21 <142540867+Mksn21@users.noreply.github.com>
2023-10-28 11:55:26 +0700

    Update main.py

--- BMW/main.py
+++ BMW/main.py
@@ -1,18 +1,53 @@
 import streamlit as st
-import numpy as np 
-import pandas as pd 
+import streamlit.components.v1 as components
+import serial
 
-st.title("Kalkulator Sederhana Streamlit")
-st.write("Operasi matematika dari dua input")
-st.sidebar.header("Operasi Matematika")
-num1 = st.number_input("Angka Pertama")
-num2 = st.number_input("Angka Kedua")
+html_string = '''
+<button>Request Serial Port</button>
+<script>
+  const button = document.querySelector('button');
+  button.addEventListener('click', async function() {
 
-if st.sidebar.button("Tambah") : 
-    st.write("Hasil ="+str(num1+num2))
-if st.sidebar.button("kurang"):
-    st.write("Hasil ="+str(num1-num2))
-if st.sidebar.button("kali"):
-    st.write("Hasil ="+str(num1*num2))
-if st.sidebar.button("Bagi"):
-    st.write("Hasil = " +str(num1/num2))
+    // Prompt user to select any serial port.
+    const port = await navigator.serial.requestPort();
+
+    // Wait for the serial port to open.
+    await port.open({ baudRate: 9600 });
+  });
+</script>
+'''
+if st.sidebar.button("add"):
+  components.html(html_string)
+    
+def serial_ports():
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+co = serial_ports()
+
+if st.sidebar.button("as"): 
+    st.write(co)
